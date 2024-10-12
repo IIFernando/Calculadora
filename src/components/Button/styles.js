911a43cdc57b7240c234c4a7535c16e53d88c7af
Fernando Araujo <fernandoaraujocontato@outlook.com>
2024-10-11 22:23:31 -0300

    first commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #CDCDCD;
+    background-color: #000000;
+    color: #FFFFFF;
+    font-size: 24px;
+    fot-weight: 700;
+    flex: 1;
+
+    &:hover {
+        Opacity: 0.7;
+    }
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div `
+    width: 100%;
+    height: 75px;
+    background-color: #AAFFAA;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    font-size: 24px;
+    font-family: 'Roboto';
+    
+    input{
+        width: 100%;
+        height: 75px;
+        background-color: #001191;
+        color: #FFFFFF;
+        border: 0;
+        float: right;
+        clear: both;
+        padding: 0 10px;
+        font-size: 50px;
+        font-family: 'Roboto';
+    }
+`
